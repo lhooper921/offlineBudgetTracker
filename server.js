f2b6df7d53382c680ada2c97cd43bbd2edbc5a49
Lauren Hooper <lluther921@gmail.com>
2020-11-06 14:49:52 -0800

    added new get

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get('/', function(req, res){
-  res.redirect('/');
+  res.redirect('./Develop/public/index.html');
 });
 
 mongoose.connect(
