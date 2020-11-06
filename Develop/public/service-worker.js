fb29dc1528aa7df73bfecee421c2d0b0bf820867
Lauren Hooper <lluther921@gmail.com>
2020-11-06 15:04:31 -0800

    removed index model, added html route

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -7,7 +7,8 @@ const FILES_TO_CACHE = [
   "./index.html",
   "./icons/icon-192x192.png",
   "./icons/icon-512x512.png",
-  "./manifest.webmanifest"
+  "./manifest.webmanifest",
+  "./favicon.ico"
  ]
   
   
