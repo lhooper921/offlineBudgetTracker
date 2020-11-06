9bbed6f2b6aaf5c117c1c7316dbd711c3f3b5a0b
Lauren Hooper <lluther921@gmail.com>
2020-11-06 14:17:52 -0800

    updated port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 
-PORT = process.env.PORT || 3000;
+PORT = process.env.PORT;
 const app = express();
 
 app.use(logger("dev"));
