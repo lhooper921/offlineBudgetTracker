ab9ea43b690e18cdfe52017a6b6a64c128b8e622
Lauren Hooper <lluther921@gmail.com>
2020-11-06 12:58:52 -0800

    errors fixed

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
-  // "./index.js",
+  "./index.js",
   "./styles.css",
   "./index.html",
   "./icons/icon-192x192.png",
